336947a8c8b782cd40d0924778d5ef16ab997847
gusdl1992 <tlgusdl92@gmail.com>
2024-03-06 19:15:02 +0900

    2024-03-06 19:14 박시현 2024-03-06 19:14 회원정보 마이페이지 html 출력 완 CSS 미완

---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,40 @@
+
+
+// 1. 로그인 여부 확인 요청 [ JS 열릴때마다 체크 ]
+$.ajax({
+    url : 'employee/login/check' ,
+    method : 'get' ,
+    success : (r)=>{
+        console.log(r);
+        // 1. 어디에
+        let login_menu = document.querySelector('#login_menu');
+        // 2. 무엇을
+        let html = '';
+        if(r != ''){ // 로그인 했을떄
+        $.ajax({
+            url : 'employee/login/checkname' ,
+            method : 'get' ,
+            data:{eno : r},
+            async : false ,
+            success : (r2) =>{
+                html = `
+                    <li class="nav-item">
+                        <a class="nav-link" onclick = "logout()">로그아웃</a>
+                    </li>
+                    <li class="nav-item">
+                        <img src="/img/${r2.uuidFile}"/> ${r2}님
+                    </li>
+                `;
+            }
+        });
+                }else{  // 로그인 안했을떄
+                    html = `
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">로그인</a>
+                        </li>
+                    `;
+                }
+                // 3. 대입
+                login_menu.innerHTML = html;
+            } // s end
+        }); // ajax end
