8e55400d5cb231ee6ca31680dc24bd05dc17e601
tlawo33 <tlawo33@naver.com>
2024-03-06 17:57:14 +0900

    2024-03-06 배열받아오기

--- src/main/resources/static/js/project/recproject.js
+++ src/main/resources/static/js/project/recproject.js
@@ -3,12 +3,12 @@
 function onWrite(){
     console.log("onWrite()")
     let pjno = document.querySelector("#pjno").value
-        let list1Elements = document.querySelectorAll(".list1");
-        let list1 = Array.from(list1Elements).map(element => parseInt(element.value));
-        let list2Elements = document.querySelectorAll(".list2");
-        let list2 = Array.from(list2Elements).map(element => parseInt(element.value));
-        let list3Elements = document.querySelectorAll(".list3");
-        let list3 = Array.from(list3Elements).map(element => parseInt(element.value));
+    let list1Elements = document.querySelectorAll(".list1");
+    let list1 = Array.from(list1Elements).map(element => element.value);
+    let list2Elements = document.querySelectorAll(".list2");
+    let list2 = Array.from(list2Elements).map(element => element.value);
+    let list3Elements = document.querySelectorAll(".list3");
+    let list3 = Array.from(list3Elements).map(element => element.value);
     console.log(pjno)
     console.log(list1)
     $.ajax({
